fe8cc01d7c515e60bfe3946e8f99a77d1e74acdd
Adithya Lekshmi <alekshmi222@gmail.com>
2026-04-06 12:02:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,345 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-master-bedroom-gf-toilet",
+      "name": "Master bedroom gf toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6265939787521972,
+          "pitch": 0.02588192530200395,
+          "rotation": 5.497787143782138,
+          "target": "1-master-bedroom-gf-toilet-shower"
+        },
+        {
+          "yaw": -1.599770361429787,
+          "pitch": 0.05488432036342594,
+          "rotation": 17.27875959474387,
+          "target": "2-rear-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-master-bedroom-gf-toilet-shower",
+      "name": "master bedroom gf toilet shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9925232250823655,
+          "pitch": 0.030925951027578336,
+          "rotation": 0.7853981633974483,
+          "target": "0-master-bedroom-gf-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-rear-toilet",
+      "name": "Rear toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.563643453854545,
+          "pitch": 0.16144707063235586,
+          "rotation": 0,
+          "target": "3-rear-toilet-shower"
+        },
+        {
+          "yaw": -1.6112395992024364,
+          "pitch": -0.04155109913230959,
+          "rotation": 0,
+          "target": "4-ff-bedroom-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-rear-toilet-shower",
+      "name": "Rear toilet shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5817001987233752,
+          "pitch": 0.05678802687904927,
+          "rotation": 0.7853981633974483,
+          "target": "2-rear-toilet"
+        },
+        {
+          "yaw": -1.3757297398832122,
+          "pitch": -0.06083669700201,
+          "rotation": 1.5707963267948966,
+          "target": "2-rear-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ff-bedroom-toilet",
+      "name": "ff bedroom toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7226863958165062,
+          "pitch": 0.04397322331331566,
+          "rotation": 0,
+          "target": "5-ff-bedroom-toilet-shower"
+        },
+        {
+          "yaw": -2.004778803614114,
+          "pitch": -0.0631957819290907,
+          "rotation": 0,
+          "target": "6-rear-bedroom-toilet--ff"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ff-bedroom-toilet-shower",
+      "name": "ff bedroom toilet shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8641328734523235,
+          "pitch": 0.09502978339238943,
+          "rotation": 0,
+          "target": "4-ff-bedroom-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-rear-bedroom-toilet--ff",
+      "name": "Rear bedroom toilet  ff",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8105914711930975,
+          "pitch": 0.1333050227878978,
+          "rotation": 5.497787143782138,
+          "target": "7-rear-bedroom-ff-toilet-shower"
+        },
+        {
+          "yaw": -1.8713614065440343,
+          "pitch": -0.07383583638321412,
+          "rotation": 0,
+          "target": "0-master-bedroom-gf-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-rear-bedroom-ff-toilet-shower",
+      "name": "rear bedroom ff toilet shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.104840222527111,
+          "pitch": 0.006118028323218283,
+          "rotation": 0.7853981633974483,
+          "target": "6-rear-bedroom-toilet--ff"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mylaudi residence - 3D Toilet interior",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
